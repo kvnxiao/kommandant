63c337ce47b866c5bece9fa80911e2768673be69
Kevin Xiao <xlzqwerty1@gmail.com>
2017-03-11 16:58:16 -0500

    added documentation for CommandContext

--- kommandant-core/src/main/java/com/github/kvnxiao/kommandant/command/CommandContext.java
+++ kommandant-core/src/main/java/com/github/kvnxiao/kommandant/command/CommandContext.java
@@ -1,16 +1,40 @@
 package com.github.kvnxiao.kommandant.command;
 
+import org.jetbrains.annotations.Nullable;
+
 /**
- * Created on:   2017-01-30
- * Author:       Kevin Xiao (github.com/alphahelix00)
+ * This class contains the context of the command, i.e. it parses a string and splits it in two based on the first
+ * available space. The first string represents the alias, and the second string represents the arguments leftover.
+ * <p>
+ * NOTE: This class was written in Java. But why you might ask? As of Kotlin 1.10, there are still some performance
+ * overhead in splitting strings by a literal. As such, string splitting in Java is currently still more performant
+ * than in Kotlin.
  */
 public class CommandContext {
 
+    /**
+     * The first string after splitting the input string.
+     */
     private final String alias;
+
+    /**
+     * The second string after splitting the input string.
+     */
     private final String args;
+
+    /**
+     * The string literal to split the incoming input string by, defined as a single space character " ".
+     */
     private static final String SPLIT_REGEX = " ";
 
-    public CommandContext(final String message) {
+    /**
+     * Constructor which takes in a string to split in two. Splits based on the first available space character.
+     * If a space does not exist, the args string will be null. If the input string is null, both resultant args and
+     * aliases will be null.
+     *
+     * @param message The message string to split into alias and args.
+     */
+    public CommandContext(@Nullable final String message) {
         if (message != null) {
             final String[] context = message.split(SPLIT_REGEX, 2);
             this.alias = (context.length > 0) ? context[0] : null;
@@ -21,18 +45,41 @@ public class CommandContext {
         }
     }
 
+    /**
+     * Gets the alias for this CommandContext. Can be null.
+     *
+     * @return The (possibly null) alias
+     */
+    @Nullable
     public String getAlias() {
         return alias;
     }
 
+    /**
+     * Gets the args for this CommandContext. Can be null.
+     *
+     * @return The (possibly null) args
+     */
+    @Nullable
     public String getArgs() {
         return args;
     }
 
+    /**
+     * A CommandContext has an alias if the alias string is not null and not empty
+     *
+     * @return whether an alias exists
+     */
     public boolean hasAlias() {
-        return alias != null;
+        return alias != null && !alias.isEmpty();
     }
 
+    /**
+     * A CommandContext has args if the args string is not null. The args can be null if the input string that was
+     * split had no space character in it.
+     *
+     * @return whether args exist
+     */
     public boolean hasArgs() {
         return args != null;
     }
